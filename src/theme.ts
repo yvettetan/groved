4e8478aa3381e398ca3452e7453040c05537546b
yvettetan <yvettetan97@gmail.com>
2022-04-07 23:04:47 +0800

    Set theme

---
+++ src/theme.ts
@@ -0,0 +1,23 @@
+export type ThemeType = {
+  color: { [key: string]: string };
+  fontFamily: { [key: string]: string };
+  fontSize: { [key: string]: string };
+};
+
+const theme: ThemeType = {
+  color: {
+    primary: "#1F3B2C",
+    primaryLight: "#588068",
+    dark: "#161C10",
+    light: "#FFF",
+  },
+  fontFamily: {
+    default: '"HKGrotesk", sans-serif',
+    headline: '"Optima", serif',
+  },
+  fontSize: {
+    default: "16px",
+  },
+};
+
+export default theme;
