5b917ac444503707c38849e34f090f29bba6068e
yvettetan <yvettetan97@gmail.com>
2022-04-07 23:07:52 +0800

    Use theme and global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Normalize } from "styled-normalize";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import GlobalStyle from "./globalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
