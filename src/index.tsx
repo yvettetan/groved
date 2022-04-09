823c4ac0e97bcbf689f9df2336a554aacdff0ca1
yvettetan <yvettetan97@gmail.com>
2022-04-09 14:52:01 +0800

    Setup page routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,36 @@ import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import GlobalStyle from "./globalStyle";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./landing/LandingContainer";
+import ShopContainer from "./shop/ShopContainer";
+import AboutContainer from "./about/AboutContainer";
+import BlogContainer from "./blog/BlogContainer";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<Landing />} />
+            <Route path="about" element={<AboutContainer />} />
+            <Route path="shop" element={<ShopContainer />} />
+            <Route path="blog" element={<BlogContainer />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
