7a1931f5b6da6feff22349714917af081188b5a7
yvettetan <yvettetan97@gmail.com>
2022-04-26 23:13:20 +0800

    Remove blog

--- src/shop/ProductContainer.tsx
+++ src/shop/ProductContainer.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getPlant } from "../data";
 import styled from "styled-components";
 import { getPlants, getMultiplePlants } from "../data";
 import ItemCard from "../shared/ItemCard";
-import { Link } from "react-router-dom";
 import ProductInfo from "./ProductInfo";
-import { getArticles, getMultipleArticles } from "../data";
 
 const SectionContainer = styled.section`
   max-width: 80rem;
@@ -24,13 +22,14 @@ const ItemList = styled.ul`
   padding: 0;
 `;
 
-const ProductContainer = () => {
+const ProductContainer: React.FC = (props) => {
+  const navigate = useNavigate();
+  // const { handle, plantName, kitName } = useParams();
   const name = useParams().plantName?.replace(/-/g, " ");
   const plant = name && getPlant(name);
   const plants = getPlants().filter((item) => item !== plant);
   const suggestedPlants = getMultiplePlants(plants, 4);
 
-  const articles = getMultipleArticles(getArticles(), 3);
   return (
     <div style={{ margin: "2rem 8rem" }}>
       <SectionContainer>{plant && <ProductInfo {...plant} />}</SectionContainer>
@@ -39,28 +38,17 @@ const ProductContainer = () => {
         <ItemList>
           {suggestedPlants.map((plant) => {
             return (
-              <Link
-                to={`/shop/${plant.name.replace(/\s/g, "-")}`}
+              <div
                 key={plant.id}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  navigate(
+                    `/shop/homebud-grove/${plant.name.replace(/\s/g, "-")}`
+                  );
+                }}
               >
                 <ItemCard {...plant} size={"small"} />
-              </Link>
-            );
-          })}
-        </ItemList>
-      </SectionContainer>
-      <SectionContainer>
-        <h2>Learn from our blog</h2>
-        <ItemList>
-          {articles.map((article) => {
-            return (
-              <Link
-                to={`/blog/${article.title.replace(/\s/g, "-")}`}
-                key={article.id}
-              >
-                <img src={article.img} alt={article.title} />
-                <p style={{ width: "100%" }}>{article.title}</p>
-              </Link>
+              </div>
             );
           })}
         </ItemList>
