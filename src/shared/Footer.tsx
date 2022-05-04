d73356308cf34546577da55f8355a85fd6808676
yvettetan <yvettetan97@gmail.com>
2022-05-04 15:14:07 +0800

    Remove footer details

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,30 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const FooterContainer = styled.section`
+const FooterContainer = styled.footer`
   background-color: ${(props) => props.theme.color.primary};
   text-align: center;
   padding: 4rem 2rem;
   display: flex;
 
   .logo {
-    color: ${(props) => props.theme.color.light};
     padding: 0 4rem;
-  }
-
-  .footer-links-container {
-    display: flex;
-  }
-
-  .footer-links {
-    display: flex;
-    flex-direction: column;
-    text-align: left;
-    padding: 0 4rem;
-    a {
-      color: ${(props) => props.theme.color.light};
-      padding: 0.5rem;
-    }
   }
 `;
 
@@ -32,18 +16,6 @@ const Footer = () => {
   return (
     <FooterContainer>
       <img className="logo" src={"/assets/images/logo-name.svg"} alt="logo" />
-      <div className="footer-links-container">
-        <div className="footer-links">
-          <a href="/">Newsletter</a>
-          <a href="/">About us</a>
-          <a href="/">Contact us</a>
-        </div>
-        <div className="footer-links">
-          <a href="/">Privacy policy</a>
-          <a href="/">Terms and conditions</a>
-          <a href="/">FAQs</a>
-        </div>
-      </div>
     </FooterContainer>
   );
 };
